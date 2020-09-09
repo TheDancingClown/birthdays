3399ed5b4efbab3521b320c6b3248de36c07d646
Louis Kirkham <louiskirkham@gmail.com>
2020-09-09 22:05:19 +0100

    added check birthday method

--- lib/birthday_list.rb
+++ lib/birthday_list.rb
@@ -1,1 +1,35 @@
+class BirthdayList
+  attr_reader :birthdays
 
+  def initialize
+    @birthdays = {}
+  end
+
+  def store(name, birthday)
+    @birthdays[name] = birthday
+  end
+
+  def show
+    @birthdays.each { |name, birthday| 
+    puts "#{birthday}: #{name}" }
+  end
+
+  def check(date=(Time.new).strftime("%d/%m"))
+    @birthdays.each do |name, birthday|
+      age = (Time.new).strftime("%Y").to_i - birthday[-4..-1].to_i
+      if birthday.start_with?(date) 
+        puts <<~HEREDOC
+          It's #{name}'s birthday today.
+          They are #{age} years old.
+        HEREDOC
+      end
+    end
+  end
+
+end
+
+list = BirthdayList.new
+list.store("alex", "09/09/1980")
+list.store("louis", "27/10/1981")
+list.show
+list.check
--- spec/birthday_list_spec.rb
+++ spec/birthday_list_spec.rb
@@ -1,1 +1,32 @@
+# I want a program that I can load in IRB that allows me to
+# - Store all of my friends’ birthdays so I can keep track of them list.store
+# - See them all at once with their names and birthdays each on a line in a tidy format list.show
+# - Check whose birthday it is today - the program can check through the birthdays I have stored and check each one to see if it’s someone’s birthday, and tells me something like "It's Mary Poppin's birthday today! They are 124 years old!" - otherwise it won't say anything.
+require 'birthday_list'
 
+describe BirthdayList do
+
+  describe '#store' do
+    it 'stores a name and birthday in a list' do
+      list = BirthdayList.new
+      list.store("name", "birthday")
+      expect(list.birthdays).to include({"name" => "birthday"})
+    end
+  end
+
+  describe '#show' do
+    it 'shows a list of names and birthdays' do
+      list = BirthdayList.new
+      list.store("name", "birthday")
+      expect{ list.show }.to output("#{"birthday"}: #{"name"}\n").to_stdout
+    end
+  end
+
+  describe '#check' do
+    it 'returns a name when passed a date' do
+      list = BirthdayList.new
+      list.store("name", "birthday")
+      expect(list.check("birthday")).to eq("name" => "birthday")
+    end
+  end
+end
